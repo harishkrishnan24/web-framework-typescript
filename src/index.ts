8eb3f0e3b1bbab5b61793e862de5f0b7925cf83d
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-03-27 12:45:30 +0530

    Add user model

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hi there");
+import { User } from "./models/User";
+
+const user = new User({ name: "My Name", age: 20 });
+
+user.set({ name: "New name" });
+
+console.log(user.get("name"));
+console.log(user.get("age"));
