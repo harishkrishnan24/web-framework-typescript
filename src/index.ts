65fb8a4334031f86a17fdcaf0654ca70042712ea
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-04 23:47:55 +0530

    Add generics to sync class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'New Record', age: 0 });
-user.save();
+
+user.events.on('change', () => {
+    console.log('change');
+});
+
+user.events.trigger('change');
