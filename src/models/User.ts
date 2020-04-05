b4128113c3d9b52615b010b6da6eb12a2965c536
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-05 12:08:18 +0530

    Add tsconfig file

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,24 +1,24 @@
-import { Eventing } from './Eventing';
-import { Sync } from './Sync';
+import { Eventing } from "./Eventing";
+import { Sync } from "./Sync";
 
 export interface UserProps {
-  id?: number;
-  name?: string;
-  age?: number;
+	id?: number;
+	name?: string;
+	age?: number;
 }
 
-const rootUrl = 'http://localhost:3000/users'
+const rootUrl = "http://localhost:3000/users";
 export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+	public events: Eventing = new Eventing();
+	public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
-  constructor(private data: UserProps) { }
+	constructor(private data: UserProps) {}
 
-  get(propName: string): string | number {
-    return this.data[propName];
-  }
+	get(propName: string): string | number {
+		return this.data[propName];
+	}
 
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
+	set(update: UserProps): void {
+		Object.assign(this.data, update);
+	}
 }
