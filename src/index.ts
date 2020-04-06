7176e61ee9af729df2fe6429bbb7ae35583877ac
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-07 00:33:37 +0530

    Implement pass through methods

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import { User } from './models/User';
+import { User } from "./models/User";
 
-const user = new User({ name: 'New Record', age: 0 });
+const user = new User({ name: "New Record", age: 0 });
 
-user.events.on('change', () => {
-    console.log('change');
+user.on("change", () => {
+	console.log("User was changed");
 });
-
-user.events.trigger('change');
