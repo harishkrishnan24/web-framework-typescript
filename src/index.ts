bbdce2f16e60d4a60606d7af868154be43017138
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-04-12 05:06:38 +0530

    Added view class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import { Collection } from "./models/Collection";
-import { User, UserProps } from "./models/User";
+import { UserForm } from "./views/UserForm";
+import { User } from "./models/User";
 
-const collection = new Collection<User, UserProps>(
-	"http://localhost:3000/users",
-	(json: UserProps) => User.buildUser(json)
-);
-collection.on("change", () => {
-	console.log(collection);
-});
-collection.fetch();
+const user = User.buildUser({ name: "NAME", age: 20 });
+
+const root = document.getElementById("#root");
+
+if (root) {
+	const userForm = new UserForm(root, user);
+	userForm.render();
+} else {
+	throw new Error("Root Element Not found!");
+}
